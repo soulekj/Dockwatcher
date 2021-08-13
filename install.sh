b807fa1b56e009752d9191757a7bcf52c5255738
soulekj <kjs243@cornell.edu>
2021-08-13 09:29:43 -0400

    Added cron script

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ sudo pip3 install pandas
 sudo pip3 install sense-hat
 sudo pip3 install feedparser
 sudo pip3 install ISStreamer
+
+croncmd = "python3 /home/pi/Dockwatcher/Dockwatcher.py"
+cronjob = "*/1 * * * * $croncmd"
+cat <(fgrep -i -v "$croncmd" <(crontab -l)) <(echo "$cronjob") | crontab -
